a383edceab900e5779ae33a629405b1f33b9537f
tadakun <kokoroko.arive@gmail.com>
2016-11-15 17:51:40 +0900

    [追加・修正]移動可能マスの表示・カーソルがキャラ上にあるだけの時と移動中で色の変更・カーソルがキャラ外の時マス透明

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class MapManager : MonoBehaviour {
 
+	PlayerController playerController;
+
 	const int m_Width = 10;
 	const int m_Height = 10;
 	public float size = 10F;
     public bool insMap = false;
+	public bool isAlpha = false;
 
     /// <summary>
     /// タイルカラー用マテリアル
     /// </summary>
     public Material[] mat;
     public int matNum = 0;
+	public float colorR = 0;
+	public float colorG = 0;
+	public float colorB = 0;
+	public float alphaA = 0;
 
 	public class Block{
 		public int height = 1; // 高さ
@@ -47,44 +54,24 @@ public class MapManager : MonoBehaviour {
 
 	public GameObject[,] panel = new GameObject[m_Width, m_Height];
 
-    /*public void DrawMap(){
+    public void DrawMap(){
 		for (int x = 0; x < m_Width; x++) {
 			for (int y = 0; y < m_Height; y++) {
-
-				panel[x,y] = Instantiate (Resources.Load ("MapTile"), new Vector3 (x * size, 0, y * size), Quaternion.identity) as GameObject;
-
-				if (block [x, y].blockNum == 1) {
-					Instantiate (Resources.Load ("Cube"), new Vector3 (x * size, 0, y * size), Quaternion.identity);
+				if (insMap == false) {
+					panel [x, y] = Instantiate (Resources.Load ("MapTile"), new Vector3 (x * size, 0, y * size), Quaternion.identity) as GameObject;
+					panel [x, y].GetComponent<Renderer> ().enabled = false;
+					if (block [x, y].blockNum == 1) {
+						Instantiate (Resources.Load ("Cube"), new Vector3 (x * size, 0, y * size), Quaternion.identity);
+					} 
 				} else if (block [x, y].movable == true) {
-					panel [x, y].GetComponent<Renderer> ().material.color = new Color(0,0.5F ,0.5F);
+					if (!isAlpha) {
+						panel [x, y].GetComponent<Renderer> ().enabled = true;
+						panel [x, y].GetComponent<Renderer> ().material.color = new Color (colorR, colorG, colorB, alphaA);
+					} else {
+						panel [x, y].GetComponent<Renderer> ().enabled = false;
+					}
 				}
 			}
 		}
-	}*/
-
-    public void DrawMap()
-    {
-        for (int x = 0; x < m_Width; x++)
-        {
-            for (int y = 0; y < m_Height; y++)
-            {
-                if (insMap == false){
-                    panel[x, y] = Instantiate(Resources.Load("MapTile"), new Vector3(x * size, 0, y * size), Quaternion.identity) as GameObject;
-
-                    if (block[x, y].blockNum == 1)
-                    {
-                        Instantiate(Resources.Load("Cube"), new Vector3(x * size, 0, y * size), Quaternion.identity);
-                    }
-                }
-
-                if (block[x, y].movable == true)
-                {
-                    // panel[x, y].GetComponent<Renderer>().material.color = new Color(0, 0.5F, 0.5F);
-                    panel[x, y].GetComponent<Renderer>().material = mat[matNum];
-                }
-            }
-        }
-    }
-
-    
+	}  
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,42 +8,42 @@ public class PlayerController : MonoBehaviour {
 	MovableScript movableScript;
 	GameObject cirsor;
 
-	public bool isMove = false;
-    public bool isUpCirsor = false;
-	public bool isPress = false;
-	public bool playButton = true;
-
-	public float keyTimer = 1.0F;
+	/// <summary>
+	/// 各種ブーリアン
+	/// </summary>
+	public bool isMove = false;			// 移動中か
+    public bool isUpCirsor = false;		// カーソルがキャラの上にいるか
+	public bool isPress = false;		// 移動ボタン選択中か
 
     /// <summary>
     /// ユニットのステータス
     /// </summary>
-    public string charaName = "Lin";
-	public int hp = 18;
-	public int attack = 7;
-	public int deffence = 4;
-	public int moveCost = 5;
+    public string charaName = "Lin";	// キャラクターの名前
+	public int hp = 18;					// 体力
+	public int attack = 7;				// 攻撃力
+	public int deffence = 4;			// 防御力
+	public int moveCost = 5;			// 移動力
 
     private int posDiv = 10; // ユニットの座標を1単位にするために割る
-
-    
-
 
 	/// <summary>
 	/// 各種メニュー処理
 	/// </summary>
 
 	void Start () {
+		// Find
 		map = GameObject.Find ("MapController").GetComponent<MapManager> ();
 		movableScript = GameObject.Find ("MapController").GetComponent<MovableScript> ();
 		cirsor = GameObject.Find ("Cirsor");
+
+		// マップの描画関係
 		map.MapInit();
 		map.DrawMap ();
-        map.insMap = true;
+        map.insMap = true;	// 初期描画完了
 	}
 
 	void Update () {
-		Debug.Log (isPress);
+		// カーソルがキャラの上に乗っているかの判定
         if(this.transform.position.x == cirsor.transform.position.x && this.transform.position.x == cirsor.transform.position.z)
         {
             isUpCirsor = true;
@@ -51,70 +51,78 @@ public class PlayerController : MonoBehaviour {
         {
             isUpCirsor = false;
         }
-		if (Input.GetKeyDown (KeyCode.Space) || Input.GetButtonDown ("A_Button")) {
-			if (!isPress) {
-				isPress = true;
-			} else {
-				isPress = false;
-			}
-		}
+
+
+		// カーソルがキャラの上にある
 		if (isUpCirsor == true) { 
-			if (isPress == true) {
-				map.matNum = 2;
-				// map.insMap = true;
-				isMove = true;
-			} else if(isPress == false){
-				map.matNum = 1;
-				// map.insMap = false;
-				isMove = false;
+			map.isAlpha = false;
+			// 移動ボタン選択中か否かの判定
+			if (Input.GetKeyDown (KeyCode.Space) || Input.GetButtonDown ("A_Button")) {
+				if (!isPress) {
+					isPress = true;
+					isMove = true;
+				} else {
+					isMove = false;
+					isPress = false;
+				}
+			}
+
+			if (!isPress) {
+				map.colorR = 0F;
+				map.colorG = 0.5F;
+				map.colorB = 0.5F;
+				map.alphaA = 0.7F;
+			} else {
+				map.colorR = 0.5F;
+				map.colorG = 1F;
+				map.colorB = 1F;
+				map.alphaA = 0.7F;
 			}
 		} else {
-			map.matNum = 0;
+			map.isAlpha = true;
+			// カーソルがキャラの上でなければ、マップチップの色をデフォルトに
+			map.colorR = 0F;
+			map.colorG = 0.5F;
+			map.colorB = 0.3F;
+			map.alphaA = 1.0F;
 		}
+
 		// 移動可能範囲の算出と色の変更
 		Vector3 pos;
 		pos = transform.position;
 		movableScript.moveSearch(Mathf.RoundToInt(pos.x) / posDiv, Mathf.RoundToInt(pos.z) / posDiv, moveCost);
 		map.DrawMap();
-		Debug.Log (map.matNum);
 	}
         
-         /*if(Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("A_Button"))
-            {
-                if (isPress == false && isUpCirsor == true)
-                {
-                isPress = true;
-                if (isMove == false)
-                {
-                    map.matNum = 1;
-                    // map.insMap = true;
-                    isMove = true;
-                }
-                else
-                {
-                    map.matNum = 0;
-                    // map.insMap = false;
-                    isMove = false;
-                }
+		/*if (Input.GetKeyDown (KeyCode.Space) || Input.GetButtonDown ("A_Button")) {
+			if (isPress == false && isUpCirsor == true) {
+				isPress = true;
+				if (isMove == false) {
+					map.matNum = 1;
+					// map.insMap = true;
+					isMove = true;
+				} else {
+					map.matNum = 0;
+					// map.insMap = false;
+					isMove = false;
+				}
                 
-            }
+			}
 			// 移動可能範囲の算出と色の変更
 			Vector3 pos;
 			pos = transform.position;
-			movableScript.moveSearch(Mathf.RoundToInt(pos.x) / posDiv, Mathf.RoundToInt(pos.z) / posDiv, moveCost);
-			map.DrawMap();
+			movableScript.moveSearch (Mathf.RoundToInt (pos.x) / posDiv, Mathf.RoundToInt (pos.z) / posDiv, moveCost);
+			map.DrawMap ();
 			isPress = false;
-        }
+		}
 
 
-		if(this.transform.position.x == cirsor.transform.position.x && this.transform.position.x == cirsor.transform.position.z)
-		{
+		if (this.transform.position.x == cirsor.transform.position.x && this.transform.position.x == cirsor.transform.position.z) {
 			isUpCirsor = true;
-		} else
-		{
+		} else {
 			isUpCirsor = false;
-		}*/
+		}
 
 
-
+	}*/
 }
