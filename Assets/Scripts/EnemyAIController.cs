18a0d7df9358697ecf7a7499150963fec7890ba6
DESKTOP-Q9DAKTP\kokor <kokoroko.arive@gmail.com>
2016-12-08 17:49:08 +0900

    [追加]EnemyController追加

---
+++ Assets/Scripts/EnemyAIController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
